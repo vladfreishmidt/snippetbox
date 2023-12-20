1e75534f20d37d214cffe66a5061c09052eb5c2d
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-12-20 14:49:38 +0200

    Remove comments

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 )
 
-// Define a home handler function which writes a byte slice containing
-// "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
@@ -31,19 +29,11 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	// Use the http.ListenAndServe() function to start a new web server.
-	// We pass in two parameters: the TCP network address to listen on
-	// (in this case ":4000") and the servemux we just created.
-	// If http.ListenAndServe() returns an error we use the log.Fatal()
-	// function to log the error message and exit. Note that any error
-	// returned by http.ListenAndServe() is always non-nil.
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
