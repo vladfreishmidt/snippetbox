a7036ad033542ca337f3c0f938956e919efc3f37
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-12-25 20:31:35 +0200

    Add templateData struct

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -76,11 +76,14 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", snippet)
+	data := &templateData{
+		Snippet: snippet,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.serverError(w, err)
 	}
-
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/vladfreishmidt/snippetbox/internal/models"
+
+type templateData struct {
+	Snippet *models.Snippet
+}
