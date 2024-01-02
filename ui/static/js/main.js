2874f0485495836d76d3a7d02510eec135233cd6
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-01-02 17:49:38 +0200

    Assign href to # if eq to current

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -2,6 +2,7 @@ var navLinks = document.querySelectorAll("nav a");
 for (var i = 0; i < navLinks.length; i++) {
 	var link = navLinks[i]
 	if (link.getAttribute('href') == window.location.pathname) {
+		link.href = "#"
 		link.classList.add("live");
 		break;
 	}
