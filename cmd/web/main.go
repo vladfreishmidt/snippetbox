3f6ad9976771625de2eebbd43998b0fea2f876c9
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-01-05 23:19:09 +0200

    Add TLS settings (non-default)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"html/template"
@@ -52,6 +53,8 @@ func main() {
 	sessionsManager.Store = mysqlstore.New(db)
 	sessionsManager.Lifetime = 12 * time.Hour
 
+	sessionsManager.Cookie.Secure = true
+
 	app := &application{
 		errorLog:        errorLog,
 		infoLog:         infoLog,
@@ -61,14 +64,19 @@ func main() {
 		sessionsManager: sessionsManager,
 	}
 
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
 	srv := &http.Server{
-		Addr:     *addr,
-		ErrorLog: errorLog,
-		Handler:  app.routes(),
+		Addr:      *addr,
+		ErrorLog:  errorLog,
+		Handler:   app.routes(),
+		TLSConfig: tlsConfig,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
